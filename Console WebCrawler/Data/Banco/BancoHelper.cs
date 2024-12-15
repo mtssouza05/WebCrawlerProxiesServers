487c6f92ecc55b8bedcddf9e21cf82b65ec9ea4c
Mattheus <mattheushenriq9@gmail.com>
2024-12-15 20:49:31 -0300

    primeiroCommit

---
+++ Console WebCrawler/Data/Banco/BancoHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+namespace Console_WebCrawler.Data.Banco
+{
+    static class BancoHelper
+    {
+
+        private static string CaminhoDb = Path.Combine(Directory.GetCurrentDirectory(), "WebCrawlerDb"); 
+        public static void CriaBanco()
+        {
+            SQLiteConnection.CreateFile(CaminhoDb);
+            string conString = $"DataSource={CaminhoDb};Version=3;";
+
+
+            using (var con = new SQLiteConnection(conString))
+            {
+                con.Open();
+
+                string criaTabela = @"
+                    CREATE TABLE IF NOT EXISTS LogExecucao (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    DataInicio DATETIME NOT NULL,
+                    DataFim DATETIME NOT NULL,
+                    QtdPagina INTEGER NOT NULL,
+                    QtdLinhas INTEGER NOT NULL,
+                    JsonCaminho TEXT NOT NULL
+                    );
+                ";
+
+
+                using (var cmd = new SQLiteCommand(criaTabela, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+
+          
+        }
+
+        public static string RetornaCaminhoDb() => CaminhoDb;
+    }
+}
